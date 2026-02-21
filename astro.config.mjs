07a96683ca67e7d711fb8bee842ff475b733e004
pipodco-byte <pipod.co@gmail.com>
2026-02-21 15:17:35 -0500

    fix: update astro.config.mjs with sitemap integration and correct site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: 'https://pipod.com',
+  site: 'https://www.pipod.co',
+  integrations: [react(), sitemap()],
   output: 'static'
 });
