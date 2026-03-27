de3b2ce8abcf24d01008ad24aaac7d0d10e021d9
pipodco-byte <pipod.co@gmail.com>
2026-03-27 17:08:20 -0500

    mermeid borrado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
-import mermaid from 'astro-mermaid';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.pipod.co',
-  integrations: [react(), sitemap(), mermaid()],
+  integrations: [react(), sitemap()],
   output: 'server',
   adapter: vercel(),
   vite: {
