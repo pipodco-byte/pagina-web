fad58e26c77ff711f720bccad0725be7a531a945
pipodco-byte <pipod.co@gmail.com>
2026-03-27 16:12:58 -0500

    vercel redeploy actualizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import mermaid from 'astro-mermaid';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.pipod.co',
